ae3e51227cd91447eedc58ed044328910537a16f
Ke Zhou <ke.zhou.usa@gmail.com>
2020-08-16 21:21:45 -0700

    update

--- popular_hashtags/twitterToSpark.py
+++ popular_hashtags/twitterToSpark.py
@@ -5,33 +5,40 @@ import requests_oauthlib
 import json
 
 ACCESS_TOKEN = '1063944351724662784-MVTRTpJk2JdoFTOl3UeVt04orBjSUJ'
-ACCESS_SECRET ='GgwSxgkQVrXfnsQXxbuon3CDmV9GERKz0H8EYfv3Db76p'
-CONSUMER_KEY =  'EL9bP53IKTBWsmNVkXeBaTLH0'
+ACCESS_SECRET = 'GgwSxgkQVrXfnsQXxbuon3CDmV9GERKz0H8EYfv3Db76p'
+CONSUMER_KEY = 'EL9bP53IKTBWsmNVkXeBaTLH0'
 CONSUMER_SECRET = 'URuSzNfTCFBq7nPyaSqAjqksECbD2vUFUMq2RfhYSjoloWPQbq'
-twitter_auth = requests_oauthlib.OAuth1(CONSUMER_KEY, CONSUMER_SECRET,ACCESS_TOKEN, ACCESS_SECRET)
+twitter_auth = requests_oauthlib.OAuth1(
+    CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
+
 
 def streamTweets():
     url = 'https://stream.twitter.com/1.1/statuses/filter.json'
-    stream_param = [('language', 'en'), ('locations', '-140.99778, 41.6751050889, -52.6480987209, 83.23324'), ('track', '#')]
-    stream_url = url + '?' + '&'.join([str(t[0]) + '=' + str(t[1]) for t in stream_param])
-	# resp is a data stream received from the Twitter APIs
+    stream_param = [('language', 'en'), ('locations',
+                                         '-140.99778, -139.06 ,48.30 ,-114.03 , 60.00'), ('track', '#')]
+    stream_url = url + '?' + \
+        '&'.join([str(t[0]) + '=' + str(t[1]) for t in stream_param])
+    # resp is a data stream received from the Twitter APIs
     resp = requests.get(stream_url, auth=twitter_auth, stream=True)
     print(stream_url, resp)
     return resp
 
+
 def twitter_to_spark(resp, tcp_conn):
-	# Read the stream continuously
+    # Read the stream continuously
     for line in resp.iter_lines():
         try:
             all_tweet = json.loads(line)
-            tweet_pure_text = all_tweet['text'] + '\n'      # pyspark can't accept stream, add '\n'
+            # pyspark can't accept stream, add '\n'
+            tweet_pure_text = all_tweet['text'] + '\n'
 
             print("Tweet pure text is : " + tweet_pure_text)
-            print ("------------------------------------------")
+            print("------------------------------------------")
             tcp_conn.send(tweet_pure_text.encode('utf-8', errors='ignore'))
         except:
             e = sys.exc_info()[0]
             print("Error is: %s" % e)
+
 
 # Main logic: make TCP connection to Spark app, forward the data stream to Spark app
 conn = None
@@ -44,4 +51,4 @@ print("Twitter end server is waiting for TCP connection...")
 conn, addr = sock.accept()
 print("Spark process connected. streaming tweets to Spark process.")
 resp = streamTweets()
-twitter_to_spark(resp,conn)
+twitter_to_spark(resp, conn)
--- popular_hashtags/visualize/app.py
+++ popular_hashtags/visualize/app.py
@@ -1,21 +1,23 @@
 import ast
-from flask import Flask,jsonify,request
+from flask import Flask, jsonify, request
 from flask import render_template
 
 app = Flask(__name__)
 words = []
 counts = []
 
+
 @app.route('/updateData', methods=['POST'])
 def update_data_from_spark():
     global words, counts
     if not request.form or 'words' not in request.form:
-        return "error",400
+        return "error", 400
     words = ast.literal_eval(request.form['words'])
     counts = ast.literal_eval(request.form['counts'])
     print("current words: " + str(words))
     print("current counts: " + str(counts))
-    return "success",201
+    return "success", 201
+
 
 @app.route('/updateChart')
 def refresh_hashtag_data():
@@ -24,15 +26,15 @@ def refresh_hashtag_data():
     print("current data: " + str(counts))
     return jsonify(sWords=words, sCounts=counts)
 
+
 @app.route("/")
 def showChart():
-    global words,counts
+    global words, counts
     counts = []
     words = []
     return render_template('chart.html', counts=counts, words=words)
-	
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050) # Can use host='localhost' if run on local machine, "0.0.0.0" if run on VM
-
-
-
+    # Can use host='localhost' if run on local machine, "0.0.0.0" if run on VM
+    app.run(host="0.0.0.0", port=5050)
